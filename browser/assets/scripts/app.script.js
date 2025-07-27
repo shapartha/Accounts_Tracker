56ba96112a61393d36793edf1185e72bf50bc18e
shapartha <shapartha@users.noreply.github.com>
2025-07-27 05:06:01 +0000

    deploy: 1dbc4d6ad2a7d8317cfe0085a018b24509d4f06b

--- browser/assets/scripts/app.script.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-    let scrollPos = this.scrollY;
-    if (scrollPos > 50) {
-        document.querySelector('.scroller').style.display = 'inherit';
-    } else {
-        document.querySelector('.scroller').style.display = 'none';
-    }
-}
--- browser/scripts-UAAHEJLK.js
+++
@@ -1,1 +0,0 @@
-window.onscroll=function(){this.scrollY>50?document.querySelector(".scroller").style.display="inherit":document.querySelector(".scroller").style.display="none"};
